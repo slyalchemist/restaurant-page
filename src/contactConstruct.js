8a01ae6995988088639f743947971064d5bf561d
slyalchemist <slyvranken@gmail.com>
2024-10-28 17:27:23 -0700

    Add comments to files
    Add comments to contactConstruct.js and menuConstruct.js to clarify code

--- src/contactConstruct.js
+++ src/contactConstruct.js
@@ -1,48 +1,58 @@
 import { emptyContainer } from "./emptyTab.js";
 
 export function contactConstruct() {
+    //call function to clear page from previously generated content
     emptyContainer();
 
+    //page title
     const titleFragment = document.createDocumentFragment();
     const title = titleFragment
         .appendChild(document.createElement("div"))
         .appendChild(document.createElement("h2"));
     title.textContent = "Contact";
-
     content.appendChild(titleFragment);
 
+    /*                      */
+    /* create contacts here */
+    /*                      */
     const fredBoy = new kittyCreator("fred", "kitty has no phone!", "123 kitty kat lane");
     const dougBoy = new kittyCreator("doug", "again!", "456 kitty kat lane");
     const hagen = new kittyCreator("hagen", "actually a dog?", "doggy daycare");
 
 }
 
+//object constructor to create contacts to minimize code
 function kittyCreator(name, phone, address){
     this.name = name;
     this.phone = phone;
     this.address = address;
 
+    //container for contact
     const contactContainer = document.createElement("div");
     content.appendChild(contactContainer);
     contactContainer.className = "kitty";
 
+    //contact name
     const contactFragment = document.createDocumentFragment();
     const contact = contactFragment
         .appendChild(document.createElement("div"))
         .appendChild(document.createElement("h3"));
     contact.textContent = this.name;
     contact.className = "contactName";
+    contactContainer.appendChild(contactFragment);
 
+    //container for contact information
     const infoContact = contactFragment
         .appendChild(document.createElement("div"))
         .appendChild(document.createElement("ul"));
+    infoContact.className = "contactInfo";
+
+    //contact phone number
     const contactPhone = infoContact.appendChild(document.createElement("li"));
     contactPhone.textContent = this.phone;
 
+    //contact address
     const contactAddress = infoContact.appendChild(document.createElement("li"));
     contactAddress.textContent = this.address;
 
-    infoContact.className = "contactInfo";
-
-    contactContainer.appendChild(contactFragment);
 }
--- src/menuConstruct.js
+++ src/menuConstruct.js
@@ -1,13 +1,78 @@
 import { emptyContainer } from "./emptyTab.js";
 
 export function menuConstruct() {
+    //call function to clear page from previously generated content
     emptyContainer();
+
+    //page title
     const titleFragment = document.createDocumentFragment();
     const title = titleFragment
         .appendChild(document.createElement("div"))
         .appendChild(document.createElement("h2"));
     title.textContent = "Menu";
-
     content.appendChild(titleFragment);
 
+    //food menu subsection title
+    const sweetFragment = document.createDocumentFragment();
+    const sweetMenu = sweetFragment
+        .appendChild(document.createElement("div"))
+        .appendChild(document.createElement("h3"));
+    sweetMenu.textContent = "Sweet Treats";
+    content.appendChild(sweetFragment);
+
+    /*                              */
+    /* create food menu items here  */
+    /*                              */
+
+    const croissant = new menuItemCreator("croissant", "3", "a classic pastry");
+
+    //drink menu subsection title
+    const drinkFragment = document.createDocumentFragment();
+    const drinkMenu = drinkFragment
+        .appendChild(document.createElement("div"))
+        .appendChild(document.createElement("h3"));
+    drinkMenu.textContent = "Cafe Drinks";
+    content.appendChild(drinkFragment);
+
+    /*                              */
+    /* create drink menu items here */
+    /*                              */
+    const matcha = new menuItemCreator("matcha", "4", "an earthy green drink");
+
+}
+
+//constructor to create menu items to minimize code
+function menuItemCreator(name, price, description){
+    this.name = name;
+    this.price = price;
+    this.description = description;
+
+    //container for entire menu item
+    const menuItemContainer = document.createElement("div");
+    content.appendChild(menuItemContainer);
+    menuItemContainer.className = "menuItemContainer";
+
+    //menu item title
+    const menuFragment = document.createDocumentFragment();
+    const menuItemName = menuFragment
+        .appendChild(document.createElement("div"))
+        .appendChild(document.createElement("h4"));
+    menuItemName.textContent = this.name;
+    menuItemContainer.appendChild(menuFragment);
+
+    //container for menu item price and description
+    const itemContainer = document.createElement("div");
+    menuItemContainer.appendChild(itemContainer);
+
+    //menu item price
+    const itemPrice = document.createElement("p");
+    itemPrice.innerHTML = `$${this.price}`;
+    itemPrice.classList = "menuItemPrice";
+    menuItemContainer.appendChild(itemPrice);
+
+    //menu item description
+    const itemDescription = document.createElement("p");
+    itemDescription.textContent = this.description;
+    itemDescription.classList = "menuItemDescription";
+    menuItemContainer.appendChild(itemDescription);
 }
